66231e782961b6df778a53010c634d7d216819e0
Sreehari K P <Sreehari.KP@gnapi.tech>
2026-06-26 14:47:11 +0530

    Created a readme file

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,15 @@ import React, {useState, useEffect, useRef} from "react";
 import "./App.css";
 
 class Node {
-  constructor(value) { //Run when a node is created
-    this.value = value; //Stores the number inside the node
-    this.left = null; // At first the node has no left child so its null
-    this.right = null; // Also starts as null
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
-// function that inserts a value into the Binary Search Tree.
-// root - current node that we are checking
-// value - number we want to insert
 const insertNode = (root, value) => {
-  // If there is no node here, create a new node and return it
   if(!root) return new Node(value);
-  // ✅ If duplicate, do nothing
   if (value === root.value) {
     return root;
   }
@@ -24,36 +19,25 @@ const insertNode = (root, value) => {
   } else {
     root.right = insertNode(root.right, value);
   }
-  // After insertion is done,we return the updated root.
   return root;
 }
 
-//Function to search a value in BST
-//root-current node
-//value - number we are searching for
 const findNode = (root, value) => {
-  // If we reach a null node,that means the value does not exist in the tree.
   if(!root) return false;
   if(value === root.value) return true;
   if(value < root.value) return findNode(root.left, value);
   return findNode(root.right, value);
 }
 
-//Function to delete a value in BST
 const deleteNode = (root, value) => {
-  //If we reach null,value doesn’t exist.Return null.
   if(!root) return null;
   if(value < root.value) {
     root.left = deleteNode(root.left, value);
   } else if(value > root.value) {
     root.right = deleteNode(root.right,value);
   } else {
-    //Three cases
-    //If no left child, Just return the right child.
     if(!root.left) return root.right;
-    //If no right child, Just return the left child.
     if(!root.right) return root.left;
-    //Find Minimum in Right Subtree Why?The smallest value in right subtree is the correct replacement.
     let minNode = root.right;
     while(minNode.left) {
       minNode = minNode.left;
@@ -64,21 +48,14 @@ const deleteNode = (root, value) => {
   return root;
 }
 
-//Function to print bst in sorted order
-//result = [] - if no second argument is provided create a new empty array(default paramter)
 const inOrderTraversal = (root, result =[]) => {
   if(!root) return result;
-  //Visit left subtree first
   inOrderTraversal(root.left, result)
-  //Add current nodes value
   result.push(root.value);
   inOrderTraversal(root.right, result)
   return result;
 }
 
-//Displaying the node
-
-//makes node clickable
 const TreeView = ({ node, onSelect }) => {
   const containerRef = useRef(null);
 
@@ -169,29 +146,10 @@ const RenderNode = ({ node, onSelect }) => {
   );
 };
 
-//Component declaration
-//export default - allows the component to be imported in main.jsx
 export default function BinarySearchTreeApp() {
-  //input-current value inside text box
-  //setInput - function to update it
   const [input, setInput] = useState("");
-  //root → the top node of the tree
-  //setRoot → updates the tree
-  //null → initially empty tree
   const [root, setRoot] = useState(null);
-  //Stores status messages like:"Inserted 10","Deleted 5","10 not found"
   const [message, setMessage] = useState("");
-  //This one loads saved tree when page loads.
-  // Load from localStorage
-  // useEffect(() => {
-  //   //Gets data saved under key "bst".If something was saved before, it returns a string.
-  //   const storedTree = localStorage.getItem("bst");
-  //   if (storedTree) {
-  //     //localStorage stores data as string.JSON.parse() converts string → object.setRoot() updates state.
-  //     setRoot(JSON.parse(storedTree));
-  //   }
-  //   //empty array [] means:Run this effect only once (when component mounts).
-  // },[])
 
   useEffect(() => {
   localStorage.removeItem("bst");
@@ -203,20 +161,8 @@ const handleSelect = (value) => {
     return [...prev, value];
   });
 };
-//To store two clicked nodes and to display the calculated relationship
 const [selectedNodes, setSelectedNodes] = useState([]);
 const [relationMessage, setRelationMessage] = useState("");
-  //Save to localstorage- Runs when root changes
-  // useEffect(() => {
-  //   if(root) {
-  //     //Converts tree object to string. Save to local storage
-  //     localStorage.setItem("bst",JSON.stringify(root));
-  //   } else {
-  //     //If tree is cleared → remove storage.
-  //     localStorage.removeItem("bst");
-  //   }
-  //   //Runs this effect every time when root changes
-  // }, [root]);
     useEffect(() => {
   if (selectedNodes.length === 2) {
     const [a, b] = selectedNodes;
@@ -238,18 +184,14 @@ const [relationMessage, setRelationMessage] = useState("");
 
     let relation = "No relation";
 
-    // 1️⃣ Direct Parent
     if (infoA.parent && infoA.parent.value === b) {
       relation = `${b} (${title.parent(b)}) -> ${a} (child)`;
     }
-    //user can select nodes in any order so that is why we give this condition
     else if (infoB.parent && infoB.parent.value === a) {
       relation = `${a} (${title.parent(a)}) -> ${b} (child)`;
     }
 
-    // 2️⃣ Ancestor Based on Level Difference
     else if (infoA.ancestors.some(n => n.value === b)) {
-      //checks if one node appears in another ancestors list
       const diff = infoA.level - infoB.level;
       
       if (diff === 2)
@@ -270,7 +212,6 @@ const [relationMessage, setRelationMessage] = useState("");
         relation = `${a} (ancestor ${diff-1} levels above) -> ${b}`;
     }
 
-    // 3️⃣ Siblings
     else if (
       infoA.parent &&
       infoB.parent &&
@@ -278,7 +219,6 @@ const [relationMessage, setRelationMessage] = useState("");
     ) {
       relation = `${a} (${title.sibling(a)}) <-> ${b} (${title.sibling(b)})`;
     }
-    // 5️⃣ Cousins
    else if (
   infoA.parent &&
   infoB.parent &&
@@ -286,14 +226,13 @@ const [relationMessage, setRelationMessage] = useState("");
   infoA.level === infoB.level
 ) {
   relation = `${a} and ${b} are cousins`;
-}// 4️⃣ Uncle / Aunt (Parent's sibling)
-// 4️⃣ Uncle / Aunt (Parent's sibling)
+}
 else if (
   infoB.parent &&
   infoA.parent &&
   infoA.parent &&
   infoB.parent &&
-  infoA.parent.parent === infoB.parent.parent && // parents are siblings
+  infoA.parent.parent === infoB.parent.parent &&
   infoA.parent.value !== infoB.parent.value &&
   Math.abs(infoA.level - infoB.level) === 1
 ) {
@@ -310,36 +249,22 @@ else if (
   }
 }, [selectedNodes]);
 
-  //This function runs when the user clicks the Insert button.
-    // It does 4 important things:
-    //   Takes value from input box
-    //   Validates it
-    //   Inserts it into the BST
-    //   Updates the UI
   const handleInsert = () => {
-  //Input is converted to number from string
-  const value = parseInt(input);
-  //if not a number function stops immediately
-  if (isNaN(value)) return;
-  // 🔥 Check duplicate before inserting
-  if (findNode(root, value)) {
-    setMessage("Duplicates are not allowed");
+    const value = parseInt(input);
+    if (isNaN(value)) return;
+    if (findNode(root, value)) {
+      setMessage("Duplicates are not allowed");
+      setInput("");
+      return;
+    }
+    const newRoot = insertNode(root, value);
+    setRoot({ ...newRoot });
+    setMessage(`Inserted ${value}`);
     setInput("");
-    return;
-  }
-  const newRoot = insertNode(root, value);
-  //Update react state
-  setRoot({ ...newRoot });
-  //Shows the message
-  setMessage(`Inserted ${value}`);
-  //Resets the input field
-  setInput("");
   };
 
-  //Delete the input value by clicking
   const handleDelete = () => {
     const value = parseInt(input);
-    //isNaN - is not a number
     if (isNaN(value)) return;
 
     const newRoot = deleteNode(root, value);
@@ -348,56 +273,46 @@ else if (
     setInput("");
   };
   const findNodeInfo = (root, value, level = 0, parent = null, path = []) => {
-  if (!root) return null;
+    if (!root) return null;
 
-  if (root.value === value) {
-    return {
-      level,
-      parent,
-      ancestors: [...path]
-    };
-  }
+    if (root.value === value) {
+      return {
+        level,
+        parent,
+        ancestors: [...path]
+      };
+    }
 
-  if (value < root.value) {
+    if (value < root.value) {
+      return findNodeInfo(
+        root.left,
+        value,
+        level + 1,
+        root,
+        [...path, root]
+      );
+    }
+
     return findNodeInfo(
-      root.left,
+      root.right,
       value,
       level + 1,
       root,
       [...path, root]
     );
-  }
-
-  return findNodeInfo(
-    root.right,
-    value,
-    level + 1,
-    root,
-    [...path, root]
-  );
-};
-  //Runs when find button is clicked
+  };
   const handleFind = () => {
     const value = parseInt(input);
     if (isNaN(value)) return;
 
     const found = findNode(root, value);
-    //if value is found then true else false
     setMessage(found ? `${value} found` : `${value} not found`);
     setInput("");
   };
-  //Runs when print button is clicked
   const handlePrint = () => {
-    //returns a sorted array
     const result = inOrderTraversal(root);
-    //converts array to string
     setMessage(`InOrder: ${result.join(", ")}`);
   };
-
-  // const clearTree = () => {
-  //   setRoot(null);
-  //   setMessage("Tree cleared");
-  // };
    return (
     <div className="bt-content" >
       <div className="bst-heading">
@@ -416,7 +331,6 @@ else if (
         <button onClick={handleDelete}>Delete</button>
         <button onClick={handleFind}>Find</button>
         <button onClick={handlePrint}>Print</button>
-        {/* <button onClick={clearTree}>Clear</button> */}
       </div>
 
       <div style={{ padding: "20px" }}>
